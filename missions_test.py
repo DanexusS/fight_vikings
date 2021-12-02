dd94a7828c8eb44b73808ad006c0234bbccc4824
DanexusS <mrcherese.lite@yandex.ru>
2021-12-02 20:41:08 +0300

    This is the first prototype of mission system, would like to make major changes

--- missions_test.py
+++ missions_test.py
@@ -4,6 +4,12 @@ class AbstractObjective:
         self.is_completed = False
         self.parent = None
 
+    def complete(self):
+        self.is_completed = True
+        self.parent.current_objective_id += 1
+        if self.parent.current_objective_id > len(self.parent.objectives):
+            self.parent.complete()
+
     def __repr__(self):
         return self.description
 
@@ -25,17 +31,38 @@ class DiscoverObjective(AbstractObjective):
 
 
 class MissionObject:
-    def __init__(self, title, description, rewards, *objectives):
-        self.objectives = list(*objectives)
+    def __init__(self, title, description, reward, objectives):
+        self.objectives = objectives
         for obj in self.objectives:
             obj.parent = self
 
         self.title = title
-        self.rewards = rewards
+        self.rewards = reward
         self.description = description
         self.is_completed = False
-        self.current_objective = self.objectives[0]
+        self.current_objective_id = 0
+
+    def complete(self):
+        pass
+
+    def current_objective(self):
+        return self.objectives[self.current_objective_id]
 
     def add(self, other):
         other.parent = self
         self.objectives.append(other)
+
+    def __repr__(self):
+        return self.title
+
+
+# тестовые возможности
+
+test_mission = MissionObject("test", "idk", 100, [KillObjective("test obj", "guardian", 10),
+                                                  KillObjective("test obj 2", "guardians", 110)])
+
+print(test_mission.current_objective())
+
+test_mission.current_objective().complete()
+
+print(test_mission.current_objective())
