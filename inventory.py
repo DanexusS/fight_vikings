f10d8bc0b2caeb07de90529564e5da9f37a98f24
DanexusS <mrcherese.lite@yandex.ru>
2021-12-02 20:09:40 +0300

    New classes and minor changes

--- inventory.py
+++ inventory.py
@@ -1,20 +1,11 @@
 from enum import Enum
 
 
-class Inventory:
-    def __init__(self, item_slots):
-        self.item_slots = item_slots
-
-
-class ItemSlot:
-    def __init__(self, item, amount):
-        self.item = item
-        self.amount = amount
-
-
 class ItemType(Enum):
+    Null = -1
     Food = 0
     Equipment = 1
+    Weapon = 2
 
 
 class Attributes(Enum):
@@ -24,20 +15,40 @@ class Attributes(Enum):
     # потом дополнить
 
 
+class Inventory:
+    def __init__(self, item_slots):
+        self.item_slots = item_slots
+
+
+class ItemSlot:
+    def __init__(self, item, amount, allowed_types=ItemType.Null):
+        self.item = item
+        self.amount = amount
+        self.allowed_types = allowed_types
+
+
 class AbstractItem:
-    def __init__(self, item_type, item_id, title):
-        self.item_type = item_type
+    def __init__(self, item_id, title):
         self.item_id = item_id
         self.title = title
 
 
 class EquipmentObject(AbstractItem):
-    def __init__(self, item_type, item_id, title, *buffs):
-        super().__init__(item_type, item_id, title)
+    def __init__(self, item_type, item_id, title, buffs, durability):
+        super().__init__(item_id, title)
 
-        self.type = ItemType.Equipment
+        self.type = item_type
         self.buffs = buffs
         self.is_stackable = False
+        self.durability = durability
+
+
+class WeaponObject(AbstractItem):
+    def __init__(self, item_id, title, durability):
+        super().__init__(item_id, title)
+
+        self.type = ItemType.Weapon
+        self.durability = durability
 
 
 class AbstractUsingItem(AbstractItem):
@@ -46,8 +57,8 @@ class AbstractUsingItem(AbstractItem):
 
 
 class FoodItem(AbstractUsingItem):
-    def __init__(self, item_type, item_id, title):
-        super().__init__(item_type, item_id, title)
+    def __init__(self, item_id, title):
+        super().__init__(item_id, title)
 
         self.type = ItemType.Food
         self.is_stackable = True
