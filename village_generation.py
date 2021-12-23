0afdddf2a8fb1fd505dcd97beae1595801a12cd2
Dasp <danaseverov593@gmail.com>
2021-12-23 19:50:20 +0300

    Вся генерация - готово.

--- village_generation.py
+++ village_generation.py
@@ -59,124 +59,144 @@ class Village:
         self.generation()
 
     def generation(self):
-        center = 4
-        # Списки крайних n линий клеток с каждой стороны
-        extreme_coords_ud = []
-        extreme_coords_lr = []
-        for i in range(self.height):
-            for j in range(self.width):
-                x = self.left + j * self.cell_size
-                y = self.top + i * self.cell_size
-                if (0 < i < MASK) and (MASK < j < self.width - MASK) and \
-                        (not f"{x + self.cell_size} {y}" in extreme_coords_ud and not f"{x - self.cell_size} {y}" in extreme_coords_ud and
-                         not f"{x + self.cell_size * 2} {y}" in extreme_coords_ud and not f"{x - self.cell_size * 2} {y}" in extreme_coords_ud and
-                         not f"{x + self.cell_size * 3} {y}" in extreme_coords_ud and not f"{x - self.cell_size * 3} {y}" in extreme_coords_ud):
-                    extreme_coords_ud.append(f"{x} {y}")
-                if (0 < j < MASK) and (MASK < i < self.height - MASK) and \
-                        (not f"{x} {y + self.cell_size}" in extreme_coords_lr and not f"{x} {y - self.cell_size}" in extreme_coords_lr and
-                         not f"{x} {y + self.cell_size * 2}" in extreme_coords_lr and not f"{x} {y - self.cell_size * 2}" in extreme_coords_lr and
-                         not f"{x} {y + self.cell_size * 3}" in extreme_coords_lr and not f"{x} {y - self.cell_size * 3}" in extreme_coords_lr):
-                    extreme_coords_lr.append(f"{x} {y}")
-        # Спавн дорог -----------------------------------------
-        # H
-        for _ in range(random.randint(MIN_ROAD, MAX_ROAD)):
-            # Возможны ошибки, хотя возможно и нет
+        # Пока не сгенерируеться без ошибки (шанс на ошибку примерно 1 к 50)
+        while True:
             try:
-                x, y = extreme_coords_lr.pop(random.randint(0, len(extreme_coords_lr) - 1)).split()
-            except Exception:
-                print(extreme_coords_lr)
-            # Прокладывание дороги в матрице
-            add_roads(self, x, y, 'y')
-        # W
-        for _ in range(random.randint(MIN_ROAD, MAX_ROAD)):
-            # Возможны ошибки, хотя возможно и нет
-            try:
-                x, y = extreme_coords_ud.pop(random.randint(0, len(extreme_coords_lr) - 1)).split()
-            except Exception:
-                print(extreme_coords_ud)
-            # Прокладывание дороги в матрице
-            add_roads(self, x, y, 'x')
-        # Нахождение клеток в которых можно спавнить площадь ратушу и дома -----------------------------------------
-        coords_for_gen = []
-        for i in range(self.height):
-            for j in range(self.width):
-                if (2 < i < self.height - 2) and (2 < j < self.width - 2):
-                    # Координаты
-                    x0, x1, x2 = self.top + (j - 1) * self.cell_size, self.top + j * self.cell_size, self.top + (j + 1) * self.cell_size
-                    y0, y1, y2 = self.top + (i - 1) * self.cell_size, self.top + i * self.cell_size, self.top + (i + 1) * self.cell_size
-                    coords_empty = [(i - 1, f"{x0} {y0}", j), (i - 1, f"{x1} {y0}", j), (i - 1, f"{x2} {y0}", j),
-                                    (i, f"{x0} {y1}", j), (i, f"{x1} {y1}", j), (i, f"{x2} {y1}", j),
-                                    (i + 1, f"{x0} {y2}", j), (i + 1, f"{x1} {y2}", j), (i + 1, f"{x2} {y2}", j)]
-                    coords_road_bool = any([any([self.board[i - 2][f"{x1} {y0 - self.cell_size}"] == 'road', self.board[i + 2][f"{x1} {y2 + self.cell_size}"] == 'road']),
-                                       any([self.board[i][f"{x0 - self.cell_size} {y1}"] == 'road', self.board[i][f"{x2 + self.cell_size} {y1}"] == 'road'])])
-                    # Заполнение клеток дорогами
-                    if all(list(map(lambda x: not bool(self.board[x[0]][x[1]]), coords_empty))) and coords_road_bool:
-                        coords_for_gen.append(coords_empty)
-        # Спавн площади -----------------------------------------
-        # Поиск места для площади
-        coords_square = None
-        while coords_square is None:
-            num = random.randrange(len(coords_for_gen))
-            test = coords_for_gen[num]
-            if (RANGE_SQUARE < test[center][0] < self.height - RANGE_SQUARE) and (RANGE_SQUARE < test[center][2] < self.width - RANGE_SQUARE):
-                coords_square = coords_for_gen.pop(num)
-        # Заполнение клеток дорогами
-        for coord in coords_square:
-            self.board[coord[0]][coord[1]] = 'road'
-        # Спавн домов -----------------------------------------
-        for coords in coords_for_gen:
-            count = 0
-            for i, coord in enumerate(coords):
-                if i == 4:
-                    cell = coord
-                elif self.board[coord[0]][coord[1]] != 'road':
-                    count += 1
-            if count == 8 and random.randint(1, 4) != 1:
-                self.board[cell[0]][cell[1]] = 'house'
-        # Спавн ратуши -----------------------------------------
-        # Выбор с какой стороны от площади будет ратуша
-        for _ in range(4):
-            x_or_y = random.choice(['x', 'y'])
-            num = random.choice([3, -3])
-            coords_txt = list(map(int, coords_square[center][1].split()))
-            # Проверка что ратуша не перекрывает не одну из дорог
-            if ((x_or_y == 'y' and \
-                    (self.board[coords_square[center][0] + num][f"{coords_txt[0]} {coords_txt[1] + CELL_SIZE * num}"] != 'road' and
-                     self.board[coords_square[center][0] + (num // 3 * 4)][f"{coords_txt[0]} {coords_txt[1] + CELL_SIZE * (num // 3 * 4)}"] != 'road') or
-                     (x_or_y == 'x' and
-                      self.board[coords_square[center][0]][f"{coords_txt[0] + CELL_SIZE * num} {coords_txt[1]}"] != 'road' and
-                      self.board[coords_square[center][0]][f"{coords_txt[0] + CELL_SIZE * (num // 3 * 4)} {coords_txt[1]}"] != 'road'))):
+                center = 4
+                # Списки крайних n линий клеток с каждой стороны
+                extreme_coords_ud = []
+                extreme_coords_lr = []
+                for i in range(self.height):
+                    for j in range(self.width):
+                        x = self.left + j * self.cell_size
+                        y = self.top + i * self.cell_size
+                        # Проверка подходит ли координаты, (крайние клетки, без углов)
+                        if (0 < i < MASK) and (MASK < j < self.width - MASK) and \
+                                (not f"{x + self.cell_size} {y}" in extreme_coords_ud and not f"{x - self.cell_size} {y}" in extreme_coords_ud and
+                                 not f"{x + self.cell_size * 2} {y}" in extreme_coords_ud and not f"{x - self.cell_size * 2} {y}" in extreme_coords_ud and
+                                 not f"{x + self.cell_size * 3} {y}" in extreme_coords_ud and not f"{x - self.cell_size * 3} {y}" in extreme_coords_ud):
+                            extreme_coords_ud.append(f"{x} {y}")
+                        # Проверка подходит ли координаты, (крайние клетки, без углов)
+                        if (0 < j < MASK) and (MASK < i < self.height - MASK) and \
+                                (not f"{x} {y + self.cell_size}" in extreme_coords_lr and not f"{x} {y - self.cell_size}" in extreme_coords_lr and
+                                 not f"{x} {y + self.cell_size * 2}" in extreme_coords_lr and not f"{x} {y - self.cell_size * 2}" in extreme_coords_lr and
+                                 not f"{x} {y + self.cell_size * 3}" in extreme_coords_lr and not f"{x} {y - self.cell_size * 3}" in extreme_coords_lr):
+                            extreme_coords_lr.append(f"{x} {y}")
+                # Спавн дорог -----------------------------------------
+                # height
+                for _ in range(random.randint(MIN_ROAD, MAX_ROAD)):
+                    x, y = extreme_coords_lr.pop(random.randint(0, len(extreme_coords_lr) - 1)).split()
+                    # Прокладывание дороги в матрице
+                    add_roads(self, x, y, 'y')
+                # width
+                for _ in range(random.randint(MIN_ROAD, MAX_ROAD)):
+                    x, y = extreme_coords_ud.pop(random.randint(0, len(extreme_coords_lr) - 1)).split()
+                    # Прокладывание дороги в матрице
+                    add_roads(self, x, y, 'x')
+                # Нахождение клеток в которых можно спавнить площадь ратушу и дома -----------------------------------------
+                coords_for_gen = []
+                for i in range(self.height):
+                    for j in range(self.width):
+                        # Входит ли в диапозон
+                        if (2 < i < self.height - 2) and (2 < j < self.width - 2):
+                            # Координаты
+                            x0, x1, x2 = self.top + (j - 1) * self.cell_size, self.top + j * self.cell_size, self.top + (j + 1) * self.cell_size
+                            y0, y1, y2 = self.top + (i - 1) * self.cell_size, self.top + i * self.cell_size, self.top + (i + 1) * self.cell_size
+                            # Пустые координаты
+                            coords_empty = [(i - 1, f"{x0} {y0}", j), (i - 1, f"{x1} {y0}", j), (i - 1, f"{x2} {y0}", j),
+                                            (i, f"{x0} {y1}", j), (i, f"{x1} {y1}", j), (i, f"{x2} {y1}", j),
+                                            (i + 1, f"{x0} {y2}", j), (i + 1, f"{x1} {y2}", j), (i + 1, f"{x2} {y2}", j)]
+                            # Проверка есть ли дороги на координатах
+                            coords_road_bool = any([any([self.board[i - 2][f"{x1} {y0 - self.cell_size}"] == 'road', self.board[i + 2][f"{x1} {y2 + self.cell_size}"] == 'road']),
+                                               any([self.board[i][f"{x0 - self.cell_size} {y1}"] == 'road', self.board[i][f"{x2 + self.cell_size} {y1}"] == 'road'])])
+                            # Заполнение клеток дорогами
+                            if all(list(map(lambda x: not bool(self.board[x[0]][x[1]]), coords_empty))) and coords_road_bool:
+                                coords_for_gen.append(coords_empty)
+                # Спавн площади -----------------------------------------
+                # Поиск места для площади
+                coords_square = None
+                while coords_square is None:
+                    num = random.randrange(len(coords_for_gen))
+                    test = coords_for_gen[num]
+                    # Если координаты входят в диапозон, то берём их
+                    if (RANGE_SQUARE < test[center][0] < self.height - RANGE_SQUARE) and (RANGE_SQUARE < test[center][2] < self.width - RANGE_SQUARE):
+                        coords_square = coords_for_gen.pop(num)
+                # Заполнение клеток дорогами
+                for coord in coords_square:
+                    self.board[coord[0]][coord[1]] = 'road'
+                # Спавн домов -----------------------------------------
+                for coords in coords_for_gen:
+                    count_not_road = 0
+                    count_not_house = 0
+                    for i, coord in enumerate(coords):
+                        # Вычисляем центр квадрата координат (куда будем ставить дом), и сколько дорог вокруг
+                        if i == 4:
+                            cell = coord
+                        else:
+                            if self.board[coord[0]][coord[1]] != 'road':
+                                count_not_road += 1
+                            if self.board[coord[0]][coord[1]] != 'house':
+                                count_not_house += 1
+                    # Если вокруг нет дорог то идём дальше
+                    if count_not_road >= 8:
+                        # Если вокруг нет домов то ставим дом, иначе с шансом 33% будет дом
+                        print(count_not_house)
+                        if (count_not_house >= 8) or (count_not_house < 8 and random.randint(1, 3) == 1):
+                            self.board[cell[0]][cell[1]] = 'house'
+                # Спавн ратуши -----------------------------------------
+                # Выбор с какой стороны от площади будет ратуша
+                for _ in range(4):
+                    x_or_y = random.choice(['x', 'y'])
+                    num = random.choice([3, -3])
+                    coords_txt = list(map(int, coords_square[center][1].split()))
+                    # Проверка что ратуша не перекрывает не одну из дорог
+                    if ((x_or_y == 'y' and \
+                            (self.board[coords_square[center][0] + num][f"{coords_txt[0]} {coords_txt[1] + CELL_SIZE * num}"] != 'road' and
+                             self.board[coords_square[center][0] + (num // 3 * 4)][f"{coords_txt[0]} {coords_txt[1] + CELL_SIZE * (num // 3 * 4)}"] != 'road') or
+                            (x_or_y == 'x' and
+                              self.board[coords_square[center][0]][f"{coords_txt[0] + CELL_SIZE * num} {coords_txt[1]}"] != 'road' and
+                              self.board[coords_square[center][0]][f"{coords_txt[0] + CELL_SIZE * (num // 3 * 4)} {coords_txt[1]}"] != 'road'))):
+                        break
+                # Заполнение клеток ратуши
+                for coord in coords_square:
+                    # Вычисление координат \/
+                    coords_txt = list(map(int, coord[1].split()))
+                    if x_or_y == 'x':
+                        coord_i = coord[0]
+                        coord_x = coords_txt[0] + CELL_SIZE * num
+                        coord_y = coords_txt[1]
+                    if x_or_y == 'y':
+                        coord_i = coord[0] + num
+                        coord_x = coords_txt[0]
+                        coord_y = coords_txt[1] + CELL_SIZE * num
+                    # Если мы впритык к площади ставим дорогу, иначе ставим ратушу
+                    if (x_or_y == 'x' and num == 3 and coords_txt[0] == int(coords_square[0][1].split()[0])) or \
+                            ((x_or_y == 'x' and num == -3 and coords_txt[0] == int(coords_square[2][1].split()[0])) or
+                             (x_or_y == 'y' and num == 3 and coord[0] == coords_square[0][0]) or
+                             (x_or_y == 'y' and num == -3 and coord[0] == coords_square[6][0])):
+                        self.board[coord_i][f"{coord_x} {coord_y}"] = 'road'
+                    else:
+                        self.board[coord_i][f"{coord_x} {coord_y}"] = 'town'
+                # Удаление домов впритык к ратуше
+                x, y = list(map(int, coords_square[center][1].split()))
+                iy = coords_square[center][0]
+                # Вычисление координат /\ \/
+                if x_or_y == 'x':
+                    x += CELL_SIZE * num
+                if x_or_y == 'y':
+                    iy += num
+                    y += CELL_SIZE * num
+                for i in range(-2, 3):
+                    for j in range(-2, 3):
+                        # Заменяем на траву если тут стоит дом
+                        if self.board[iy + i][f"{x + CELL_SIZE * j} {y + CELL_SIZE * i}"] == 'house':
+                            self.board[iy + i][f"{x + CELL_SIZE * j} {y + CELL_SIZE * i}"] = 'grass'
+                # Если всё сработало - выходим из цикла
                 break
-        # Заполнение клеток ратуши
-        for coord in coords_square:
-            coords_txt = list(map(int, coord[1].split()))
-            if x_or_y == 'x':
-                coord_i = coord[0]
-                coord_x = coords_txt[0] + CELL_SIZE * num
-                coord_y = coords_txt[1]
-            if x_or_y == 'y':
-                coord_i = coord[0] + num
-                coord_x = coords_txt[0]
-                coord_y = coords_txt[1] + CELL_SIZE * num
-            if (x_or_y == 'x' and num == 3 and coords_txt[0] == int(coords_square[0][1].split()[0])) or \
-                    ((x_or_y == 'x' and num == -3 and coords_txt[0] == int(coords_square[2][1].split()[0])) or
-                     (x_or_y == 'y' and num == 3 and coord[0] == coords_square[0][0]) or
-                     (x_or_y == 'y' and num == -3 and coord[0] == coords_square[6][0])):
-                self.board[coord_i][f"{coord_x} {coord_y}"] = 'road'
-            else:
-                self.board[coord_i][f"{coord_x} {coord_y}"] = 'town'
-        # Удаление домов впритык к ратуше
-        x, y = list(map(int, coords_square[center][1].split()))
-        iy = coords_square[center][0]
-        if x_or_y == 'x':
-            x += CELL_SIZE * num
-        if x_or_y == 'y':
-            iy += num
-            y += CELL_SIZE * num
-        for i in range(-2, 3):
-            for j in range(-2, 3):
-                if self.board[iy + i][f"{x + CELL_SIZE * j} {y + CELL_SIZE * i}"] == 'house':
-                    self.board[iy + i][f"{x + CELL_SIZE * j} {y + CELL_SIZE * i}"] = 'grass'
+            except Exception:
+                # Если ошибка - попробовать ещё раз
+                print('Ошибка')
+                pass
         # ---
 
 
@@ -187,12 +207,16 @@ class Village:
         for i in range(self.height):
             for _ in range(self.width):
                 coords = (x, y, self.cell_size, self.cell_size)
+                # Дом
                 if self.board[i][f"{x} {y}"] == 'house':
                     pygame.draw.rect(screen, COLOR_HOUSE, coords)
+                # Дорога
                 elif self.board[i][f"{x} {y}"] == 'road':
                     pygame.draw.rect(screen, COLOR_ROAD, coords)
+                # Ратуша
                 elif self.board[i][f"{x} {y}"] == 'town':
                     pygame.draw.rect(screen, COLOR_TOWN, coords)
+                # Трава
                 else:
                     pygame.draw.rect(screen, COLOR_GRASS, coords)
                 x += self.cell_size
@@ -201,7 +225,7 @@ class Village:
 
 # Создание поля
 village = Village(N, N)
-# основной цикл
+# Основной цикл
 running = True
 while running:
     for event in pygame.event.get():
