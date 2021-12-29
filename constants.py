635b7202737fa7118aa30c2dae8fab3593d8ba0f
Dasp <danaseverov593@gmail.com>
2021-12-29 14:10:44 +0300

    Добавлены новые константы

--- constants.py
+++ constants.py
@@ -1,3 +1,17 @@
+import random
+
+# Общие
 BG = '#43485E'
 BG_BTN = '#C8D1F7'
 BG_BTN_SHADOW = '#242429'
+# Игрок
+PLAYER_SIZE = 40
+STEP = 4
+# Генерация
+N = random.randrange(20, 30, 2)
+EMPTY_N = N // 2
+CELL_SIZE = 100
+MIN_ROAD = N // 10
+MAX_ROAD = MIN_ROAD * 2
+MASK = N // 5
+RANGE_SQUARE = N // 2.5
