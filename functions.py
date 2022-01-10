1fc860e54d92a0bf89747d4b29e81f9bcd5806c1
Dasp <danaseverov593@gmail.com>
2022-01-10 23:55:57 +0300

    Добавлена новая функция

--- functions.py
+++ functions.py
@@ -1,7 +1,13 @@
 import pygame
 import os
 
+from constants import *
+
 
 def load_image(name):
     fullname = os.path.join('images/', name)
     return pygame.image.load(fullname).convert_alpha()
+
+
+def size_img(name, num=1):
+    return pygame.transform.scale(load_image(name), (CELL_SIZE * num, CELL_SIZE * num))
