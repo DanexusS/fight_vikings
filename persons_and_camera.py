5247df4dffbf4aea268f0a5d5067ac003f7daead
Dasp <danaseverov593@gmail.com>
2022-01-11 00:56:26 +0300

    Враг отображается. Сделать передвижение, удар и получение урона.

--- persons_and_camera.py
+++ persons_and_camera.py
@@ -20,14 +20,23 @@ class Sword(pygame.sprite.Sprite):
         self.image = pygame.transform.rotate(self.image, int(angle))
         return angle
 
-    def attack(self, dmg, groups):
+    def attack(self, dmg, other):
         # Если соприкосаеться с объектом - удар
-        for group in groups:
-            obj = pygame.sprite.spritecollideany(self, group)
+        for group in [other.houses_sprites, other.townhall_sprites, other.enemies_sprites]:
+            obj = None
+            for sprite in group:
+                if pygame.sprite.collide_mask(self, sprite):
+                    obj = sprite
+                    print(obj.__class__.__name__)
+                    break
             if obj:
                 # Бьёт 1 раз
                 if not obj.is_dmg:
-                    obj.damage(dmg)
+                    status = obj.damage(dmg)
+                    if obj.__class__.__name__ == 'Enemy' and status == 'destroed':
+                        other.enemies_sprites.remove(obj)
+                        other.enemies.remove(obj)
+
             else:
                 # Перезагрузка удара
                 for sprite in group:
@@ -94,33 +103,30 @@ class Hero(pygame.sprite.Sprite):
             r = 45
             x = WIDTH // 2
             y = HEIGHT // 2
-            step_angle = 1
-            min_coeff = 0.017
-            max_coeff = (min_coeff * (360 // step_angle))
             # Формула
             sqrt1 = math.sqrt((mouse_x - x + 1) * (mouse_x - x + 1) + (mouse_y - y + 1) * (mouse_y - y + 1))
             sqrt2 = math.sqrt(0 * 0 + 10 * 10)
             angle = round(-(90 - math.acos(math.cos(((mouse_x - x) * 0 + (mouse_y - y) * 10) / ((sqrt1 * sqrt2) + 1))) * 100))
             # Выравнивание градуса угла
-            if mouse_x <= WIDTH // 2 and mouse_y <= HEIGHT // 2:
+            if mouse_x <= x and mouse_y <= y:
                 angle = (270 - angle) - 80
-                angle_move = min_coeff * abs(angle // step_angle)
-            elif mouse_x <= WIDTH // 2 and mouse_y >= HEIGHT // 2:
+                angle_move = MIN_COEFF * abs(angle // STEP_ANGLE)
+            elif mouse_x <= x and mouse_y >= y:
                 angle = angle - 10
-                angle_move = max_coeff - (min_coeff * abs(angle // step_angle))
-            elif mouse_x >= WIDTH // 2 and mouse_y <= HEIGHT // 2:
+                angle_move = MAX_COEFF - (MIN_COEFF * abs(angle // STEP_ANGLE))
+            elif mouse_x >= x and mouse_y <= y:
                 angle -= 190
-                angle_move = max_coeff - (min_coeff * abs(angle // step_angle))
-            elif mouse_x >= WIDTH // 2 and mouse_y >= HEIGHT // 2:
+                angle_move = MAX_COEFF - (MIN_COEFF * abs(angle // STEP_ANGLE))
+            elif mouse_x >= x and mouse_y >= y:
                 angle = (90 - angle) - 80
-                angle_move = min_coeff * abs(angle // step_angle)
+                angle_move = MIN_COEFF * abs(angle // STEP_ANGLE)
             # Отрисовка
             self.sword.image = pygame.transform.rotate(self.sword.image, angle)
             self.sword.rect = self.sword.image.get_rect()
             self.sword.rect.x = x + r * math.sin(angle_move)
             self.sword.rect.y = y + r * math.cos(angle_move)
             # Проверка на атаку
-            self.sword.attack(dmg, [other.houses_sprites, other.townhall_sprites])
+            self.sword.attack(dmg, other)
         # Круговая атака
         elif bool(self.angle_attack_range):
             # Переменные
@@ -136,7 +142,7 @@ class Hero(pygame.sprite.Sprite):
             self.angle_attack_range -= step_angle
             self.angle_move -= step_coeff
             # Проверка на атаку
-            self.sword.attack(dmg, [other.houses_sprites, other.townhall_sprites])
+            self.sword.attack(dmg, other)
             if self.angle_attack_range == 0:
                 self.attack_default = True
         else:
@@ -179,32 +185,56 @@ class Hero(pygame.sprite.Sprite):
                 self.directions[(STEP, 0)] = False
 
 
+
+
 class Enemy(pygame.sprite.Sprite):
     def __init__(self, other, pos):
-        super().__init__(other.player_sprites, other.all_sprites)
+        super().__init__(other.all_sprites)
         # Переменные
         self.hp = 50
         self.status = 'normal'
         self.image = pygame.transform.scale(load_image('enemy.png'), (PLAYER_SIZE, PLAYER_SIZE))
+        self.mask = pygame.mask.from_surface(self.image)
         self.rect = self.image.get_rect()
-        self.rect.x = int(pos[0]) * CELL_SIZE
-        self.rect.y = int(pos[1]) * CELL_SIZE
+        self.rect.x = int(pos[0]) + CELL_SIZE // 2 - PLAYER_SIZE // 2
+        self.rect.y = int(pos[1]) + CELL_SIZE // 2 - PLAYER_SIZE // 2
+        self.is_dmg = False
 
     def damage(self, dmg):
         if self.status == 'normal':
+            self.is_dmg = True
             self.hp -= dmg
             if self.hp < 1:
                 self.status = 'destroed'
                 self.hp = 0
         return self.status
 
-    def attack(self, other):
-        pass
-
     def update(self, player):
+        self.rect = self.rect.move(0, 0)
         distance = math.sqrt((self.rect.x - player.rect.x) ** 2 + (self.rect.x - player.rect.x) ** 2)
         if distance < 1000:
-            self.rect = self.rect.move(x, y)
+            x = WIDTH // 2
+            y = HEIGHT // 2
+            # Формула
+            sqrt1 = math.sqrt((self.rect.x - x + 1) * (self.rect.x - x + 1) + (self.rect.y - y + 1) * (self.rect.y - y + 1))
+            sqrt2 = math.sqrt(0 * 0 + 10 * 10)
+            angle = round(-(90 - math.acos(math.cos(((self.rect.x - x) * 0 + (self.rect.y - y) * 10) / ((sqrt1 * sqrt2) + 1))) * 100))
+            # Выравнивание градуса угла
+            if self.rect.x <= x and self.rect.y <= y:
+                angle = (270 - angle) - 80
+                angle_move = MIN_COEFF * abs(angle // STEP_ANGLE)
+            elif self.rect.x <= x and self.rect.y >= y:
+                angle = angle - 10
+                angle_move = MAX_COEFF - (MIN_COEFF * abs(angle // STEP_ANGLE))
+            elif self.rect.x >= x and self.rect.y <= y:
+                angle -= 190
+                angle_move = MAX_COEFF - (MIN_COEFF * abs(angle // STEP_ANGLE))
+            elif self.rect.x >= x and self.rect.y >= y:
+                angle = (90 - angle) - 80
+                angle_move = MIN_COEFF * abs(angle // STEP_ANGLE)
+            # Отрисовка
+            self.rect.x = 0
+            self.rect.y = 0
 
 
 class Camera:
