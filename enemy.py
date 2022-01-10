85a4799a31c688ed1ff5a1b373fff3fbe37cdb50
Dasp <danaseverov593@gmail.com>
2022-01-10 19:05:15 +0300

    Файл с врагом

--- enemy.py
+++ enemy.py
@@ -6,20 +6,15 @@ from functions import *
 
 
 class Hero(pygame.sprite.Sprite):
-    def __init__(self, other, n):
+    def __init__(self, other, pos):
         super().__init__(other.player_sprites, other.all_sprites)
-        pos = random.choice([(n // 4, n // 2), (n // 4 * 3, n // 2), (n // 2, n // 4), (n // 2, n // 4 * 3)])
         # Переменные
-        self.hp = 10
+        self.hp = 50
         self.status = 'normal'
         self.image = pygame.transform.scale(load_image('enemy.png'), (PLAYER_SIZE, PLAYER_SIZE))
-        self.attack_default = True
-        self.angle_attack_range = 0
-        self.angle_move = 0
         self.rect = self.image.get_rect()
         self.rect.x = int(pos[0]) * CELL_SIZE
         self.rect.y = int(pos[1]) * CELL_SIZE
-        self.run = 1
 
     def damage(self, dmg):
         if self.status == 'normal':
@@ -27,14 +22,11 @@ class Hero(pygame.sprite.Sprite):
             if self.hp < 1:
                 self.status = 'destroed'
                 self.hp = 0
-            return self.status
-        return None
-
-    def move_player(self, direction, other):
-        pass
+        return self.status
 
     def attack(self, other):
         pass
 
-    def update(self, event):
-        pass
+    def update(self, player):
+        distance = math.sqrt((self.rect.x - player.rect.x) ** 2 + (self.rect.x - player.rect.x) ** 2)
+        if distance < 1000:
