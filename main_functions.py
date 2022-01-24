1be4743bb9cc6c866ac55d225fbcd725a05335c1
Dasp <danaseverov593@gmail.com>
2022-01-24 23:59:10 +0300

    Изменена функция для атаки

--- main_functions.py
+++ main_functions.py
@@ -12,13 +12,13 @@ def size_img(name, num=1):
     return pygame.transform.scale(load_image(name), (GAME_CELL_SIZE * num, GAME_CELL_SIZE * num))
 
 
-def step_and_draw_attack(creature, size_step_attack):
+def step_and_draw_attack(creature, size_step_attack, center):
     radius = 45
     # Отрисовка
     creature.weapon.image = pygame.transform.rotate(creature.weapon.image, creature.angle_attack_range)
     creature.weapon.rect = creature.weapon.image.get_rect()
-    creature.weapon.rect.x = PLAYER_CENTER.x + radius * math.sin(creature.angle_attack_move)
-    creature.weapon.rect.y = PLAYER_CENTER.y + radius * math.cos(creature.angle_attack_move)
+    creature.weapon.rect.x = center.x + radius * math.sin(creature.angle_attack_move)
+    creature.weapon.rect.y = center.y + radius * math.cos(creature.angle_attack_move)
     # Шаг
     creature.angle_attack_range -= STEP_ANGLE * size_step_attack
     creature.angle_attack_move -= MIN_COE * size_step_attack
